0700d9a0ef20799c5f689d74982cd0eb9fa13262
elyuu <49287229+elyuu@users.noreply.github.com>
2021-07-20 16:15:18 -0700

    added GLAD

--- src/ogl.c
+++ src/ogl.c
@@ -3,6 +3,7 @@
 ogl::ogl.c
 ===========================================================================
 */
+#include <glad/gl.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
@@ -11,26 +12,46 @@ ogl::ogl.c
 
 int main(void)
 {
-    struct SDL_Window* window = SDL_CreateWindow(
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_Window* window = SDL_CreateWindow(
         "ogl",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         800,
         600,
-        SDL_WINDOW_OPENGL);
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
-    SDL_Event e;
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    int version = gladLoadGL((GLADloadfunc) SDL_GL_GetProcAddress);
+    printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+    SDL_Event event;
     bool quit = false;
-
     while (!quit)
     {
-        while (SDL_PollEvent(&e) != 0)
+        while (SDL_PollEvent(&event))
         {
-            if (e.type == SDL_QUIT)
+            switch (event.type)
+            {
+            case SDL_QUIT:
                 quit = true;
+                break;
+            default:
+                break;
+            }
         }
     }
 
+    SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
